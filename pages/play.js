4935d8fbd2ef906409175ac220304b5cfaad5b74
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-27 06:52:15 +0700

    Page Play.

---
+++ pages/play.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+class Play extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>มาเล่นกัน</h1>
+      </div>
+    );
+  }
+}
+
+export default Play;
