506cb85ee68a78264d133eaf4ed24ca671a14266
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-27 07:07:14 +0700

    Edit Page Play.

--- pages/play.js
+++ pages/play.js
@@ -2,11 +2,23 @@ import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
 
+const Butt = styled.button`
+  background-color: green;
+  color: yellow;
+`;
+
 class Play extends React.Component {
   render() {
     return(
       <div>
-        <h1>มาเล่นกัน</h1>
+        <div>
+          <Link href="/"><Butt className="is-primary">Home</Butt></Link>
+          <Link href="/play"><Butt className="is-primary">Play</Butt></Link>
+        </div>
+        <h1>Lets play bro</h1>
+        <div>
+          <a href="https://github.com/lebrancconvas/Next-Workshop"><Butt>My Repo Ok!</Butt></a>
+        </div>
       </div>
     );
   }
