f2b60ba99727b2a15d62076a8d5da1ea9a730396
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-27 07:07:46 +0700

    Add link to change between pages and Github link.

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import Link from 'next/link';
 import styled from 'styled-components';
+import Link from 'next/link';
+
 
 const Butt = styled.button`
   background-color: green;
+  color: yellow;
 `;
 
 class Index extends React.Component {
   render(){
     return(
       <div>
-        <Link href="/play"><Butt className="is-primary">TWO</Butt></Link>
+        <div>
+          <Link href="/"><Butt className="is-primary">Home</Butt></Link>
+          <Link href="/play"><Butt className="is-primary">Play</Butt></Link>
+        </div>
         <h1>Hello Next</h1>
+        <div>
+          <a href="https://github.com/lebrancconvas/Next-Workshop"><Butt>My Repo Ok!</Butt></a>
+        </div>
       </div>
     );
   }
