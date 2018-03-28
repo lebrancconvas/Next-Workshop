d25a2ba7e968ffafab0a3be2ed7096e2ba410ed5
Lebranc Convas <lebrancconvas@gmail.com>
2018-03-29 01:06:33 +0700

    Add Logo Component to Page.

--- pages/play.js
+++ pages/play.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
+import Logo from '../components/logo.js';
 
 const Butt = styled.button`
   background-color: green;
   color: yellow;
 `;
 
+
 class Play extends React.Component {
   render() {
     return(
       <div>
         <div>
-          <Link href="/"><Butt className="is-primary">Home</Butt></Link>
-          <Link href="/play"><Butt className="is-primary">Play</Butt></Link>
+          <Link href="/"><Butt>Home</Butt></Link>
+          <Link href="/play"><Butt>Play</Butt></Link>
         </div>
+        <Logo />
         <h1>Lets play bro</h1>
         <div>
           <a href="https://github.com/lebrancconvas/Next-Workshop"><Butt>My Repo is here!</Butt></a>
